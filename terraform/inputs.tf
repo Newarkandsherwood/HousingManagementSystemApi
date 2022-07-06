78fc5b64a831a3351bd169b046d7cd94a86841e2
jamiejordanmt <jamie.jordan@madetech.com>
2022-07-06 14:08:56 +0100

    Adds Terraform

---
+++ terraform/inputs.tf
@@ -0,0 +1,35 @@
+variable "service_plan_name" {
+    type = string
+}
+
+variable "resource_group_name" {
+    type = string
+}
+
+variable "resource_group_location" {
+    type = string
+}
+
+variable "app_service_name" {
+    type = string
+}
+
+variable "authentication_identifier" {
+    type = string
+}
+
+variable "jwt_secret_production" {
+    type = string
+}
+
+variable "jwt_secret_staging" {
+    type = string
+}
+
+variable "sentry_dsn" {
+    type = string
+}
+
+variable "ancm_additional_error_page_link" {
+    type = string
+}
