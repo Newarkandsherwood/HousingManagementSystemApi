7ca0c1bc651a788b4050ceb4a0429c8acac33e0a
jamiejordanmt <jamie.jordan@madetech.com>
2022-07-07 15:32:55 +0100

    Restricts infra workflow to pushes to main and adds pre-requisites to deploy staging

--- terraform/app_service.tf
+++ terraform/app_service.tf
@@ -1,43 +1,42 @@
 data "azurerm_service_plan" "hro-app-service-plan" {
-    name                = var.service_plan_name
-    resource_group_name = var.resource_group_name
+  name                = var.service_plan_name
+  resource_group_name = var.resource_group_name
 }
 
 resource "azurerm_windows_web_app_slot" "hro-management-api-staging-slot" {
-    name           = "staging"
-    app_service_id = azurerm_windows_web_app.hro-management-api.id
+  name           = "staging"
+  app_service_id = azurerm_windows_web_app.hro-management-api.id
 
-    app_settings = {
-        ASPNETCORE_ENVIRONMENT    = "Staging"
-        AUTHENTICATION_IDENTIFIER = var.authentication_identifier
-        JWT_SECRET                = var.jwt_secret_staging
-        SENTRY_DSN                = var.sentry_dsn
-    }
+  app_settings = {
+    ASPNETCORE_ENVIRONMENT    = "Staging"
+    AUTHENTICATION_IDENTIFIER = var.authentication_identifier
+    JWT_SECRET                = var.jwt_secret_staging
+    SENTRY_DSN                = var.sentry_dsn
+  }
+  auth_settings {
+    enabled = false
+  }
 
-    auth_settings {
-        enabled = false
-    }
-
-    site_config {}
+  site_config {}
 }
 
 resource "azurerm_windows_web_app" "hro-management-api" {
-    name                = var.app_service_name
-    location            = var.resource_group_location
-    resource_group_name = var.resource_group_name
-    service_plan_id     = data.azurerm_service_plan.hro-app-service-plan.id
+  name                = var.app_service_name
+  location            = var.resource_group_location
+  resource_group_name = var.resource_group_name
+  service_plan_id     = data.azurerm_service_plan.hro-app-service-plan.id
 
-    auth_settings {
-        enabled = false
-    }
+  auth_settings {
+    enabled = false
+  }
 
-    site_config {}
+  site_config {}
 
-    app_settings = {
-        ASPNETCORE_ENVIRONMENT = "Production"
-        ANCM_ADDITIONAL_ERROR_PAGE_LINK = var.ancm_additional_error_page_link
-        AUTHENTICATION_IDENTIFIER = var.authentication_identifier
-        JWT_SECRET = var.jwt_secret_production
-        SENTRY_DSN = var.sentry_dsn
-    }
+  app_settings = {
+    ASPNETCORE_ENVIRONMENT          = "Production"
+    ANCM_ADDITIONAL_ERROR_PAGE_LINK = var.ancm_additional_error_page_link
+    AUTHENTICATION_IDENTIFIER       = var.authentication_identifier
+    JWT_SECRET                      = var.jwt_secret_production
+    SENTRY_DSN                      = var.sentry_dsn
+  }
 }
