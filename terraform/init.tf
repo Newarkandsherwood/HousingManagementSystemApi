bccf126cb09cab5f07b5494a8bb2d203c870a440
Renny Fadoju <32823756+Renny4Real@users.noreply.github.com>
2022-07-22 11:51:17 +0100

    Revert "Attempt to break terraform to test format step works"
    This reverts commit 757b3a0dbedbf21988ac147ef8837c91804c7a81.
    The commit was used to test that the format terrform step for PRs is working as it should.

--- terraform/init.tf
+++ terraform/init.tf
@@ -1,9 +1,9 @@
 terraform {
   required_providers {
-                    azurerm = {
-                      source = "hashicorp/azurerm"
-                      version = "=3.10.0"
-                    }
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=3.10.0"
+    }
   }
   backend "azurerm" {}
 }
