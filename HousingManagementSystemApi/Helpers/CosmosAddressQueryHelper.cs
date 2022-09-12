52c8f9bfb8d5aef24ec67604bf1fb37643e04d7b
Mark Warriner <mark.warriner@madetech.com>
2022-09-12 13:55:07 +0100

    Formatting Update

--- HousingManagementSystemApi/Helpers/CosmosAddressQueryHelper.cs
+++ HousingManagementSystemApi/Helpers/CosmosAddressQueryHelper.cs
@@ -1,21 +1,22 @@
-namespace HousingManagementSystemApi.Helpers;
-
-using HACT.Dtos;
-using Microsoft.Azure.Cosmos;
-
-public class CosmosAddressQueryHelper : ICosmosAddressQueryHelper
+namespace HousingManagementSystemApi.Helpers
 {
-    private readonly Container cosmosContainer;
-    public CosmosAddressQueryHelper(Container container) => this.cosmosContainer = container;
+    using HACT.Dtos;
+    using Microsoft.Azure.Cosmos;
 
-    public FeedIterator<PropertyAddress> GetItemQueryIterator<T>(string postcode) =>
-        this.cosmosContainer.GetItemQueryIterator<PropertyAddress>(GetQueryDefinition(postcode));
-
-    private static QueryDefinition GetQueryDefinition(string postcode)
+    public class CosmosAddressQueryHelper : ICosmosAddressQueryHelper
     {
-        var query =
-            "SELECT * FROM c WHERE (UPPER(REPLACE(c.PostalCode, ' ','')))  = (UPPER(REPLACE(@postcode, ' ','')))  ORDER BY c.AddressLine[0] ASC";
-        return new QueryDefinition(query)
-            .WithParameter("@postcode", postcode);
+        private readonly Container cosmosContainer;
+        public CosmosAddressQueryHelper(Container container) => this.cosmosContainer = container;
+
+        public FeedIterator<PropertyAddress> GetItemQueryIterator<T>(string postcode) =>
+            this.cosmosContainer.GetItemQueryIterator<PropertyAddress>(GetQueryDefinition(postcode));
+
+        private static QueryDefinition GetQueryDefinition(string postcode)
+        {
+            var query =
+                "SELECT * FROM c WHERE (UPPER(REPLACE(c.PostalCode, ' ','')))  = (UPPER(REPLACE(@postcode, ' ','')))  ORDER BY c.AddressLine[0] ASC";
+            return new QueryDefinition(query)
+                .WithParameter("@postcode", postcode);
+        }
     }
 }
