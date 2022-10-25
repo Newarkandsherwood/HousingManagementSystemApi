3108177ac3827059415b8a2ab798a9941cceeeed
Renny Fadoju <32823756+Renny4Real@users.noreply.github.com>
2022-10-25 12:39:56 +0100

    Add gateway test for returning communal addresses
    Co-authored-by: Mark Warriner <104864394+markWarr@users.noreply.github.com>
    Co-authored-by: Agnieszka Zbikowska <agnieszka.zbikowska@madetech.com>

--- HousingManagementSystemApi.Tests/GatewaysTests/AddressesCosmosGatewayTests.cs
+++ HousingManagementSystemApi.Tests/GatewaysTests/AddressesCosmosGatewayTests.cs
@@ -13,25 +13,32 @@ namespace HousingManagementSystemApi.Tests.GatewaysTests
     using Moq;
     using Xunit;
 
-    public class AddressesCosmosGatewayCosmosQueryHelperTests
+    public class AddressesCosmosGatewayTests
     {
-        private AddressesCosmosGateway systemUnderTest;
+        private readonly AddressesCosmosGateway systemUnderTest;
 
-        private Mock<IContainerResolver> containerResolverMock;
-        private Mock<FeedIterator<PropertyAddress>> feedIteratorMock;
+        private readonly Mock<IContainerResolver> containerResolverMock;
+        private readonly Mock<FeedIterator<PropertyAddress>> feedIteratorMock;
         private readonly Mock<ICosmosAddressQueryHelper> cosmosQueryHelperMock;
-        private readonly Mock<Container> containerMock;
+        private readonly Mock<Container> tenantContainerMock;
+        private readonly Mock<FeedResponse<PropertyAddress>> feedResponseMock;
+        private readonly Mock<Container> communalContainerMock;
 
         private const string MockPostcode = "NG21 9LQ";
-        private const string MockRepairType = "TENANT";
+        private const string MockTenantRepairType = "TENANT";
+        private const string MockCommunalRepairType = "COMMUNAL";
+        private const string TenantAddress = "ABC Tenant House";
+        private const string CommunalAddress = "Block ABC: Communal House";
 
-        public AddressesCosmosGatewayCosmosQueryHelperTests()
+        public AddressesCosmosGatewayTests()
         {
-            containerMock = new Mock<Container>();
+            tenantContainerMock = new Mock<Container>();
+            communalContainerMock = new Mock<Container>();
             containerResolverMock = new Mock<IContainerResolver>();
             systemUnderTest = new AddressesCosmosGateway(containerResolverMock.Object);
             cosmosQueryHelperMock = new Mock<ICosmosAddressQueryHelper>();
             feedIteratorMock = new Mock<FeedIterator<PropertyAddress>>();
+            feedResponseMock = new Mock<FeedResponse<PropertyAddress>>();
         }
 
         [Fact]
@@ -41,10 +48,10 @@ namespace HousingManagementSystemApi.Tests.GatewaysTests
         {
             // Arrange
             feedIteratorMock.Setup(_ => _.HasMoreResults).Returns(false);
-            containerResolverMock.Setup(x => x.Resolve(It.IsAny<string>())).Returns(containerMock.Object);
+            containerResolverMock.Setup(x => x.Resolve(It.IsAny<string>())).Returns(tenantContainerMock.Object);
             cosmosQueryHelperMock.Setup(_ => _.GetItemQueryIterator<PropertyAddress>(MockPostcode))
                 .Returns(feedIteratorMock.Object);
-            containerMock
+            tenantContainerMock
                 .Setup(c => c.GetItemQueryIterator<PropertyAddress>(
                     It.IsAny<QueryDefinition>(),
                     It.IsAny<string>(),
@@ -52,7 +59,7 @@ namespace HousingManagementSystemApi.Tests.GatewaysTests
                 .Returns(feedIteratorMock.Object);
 
             // Act
-            Func<Task> act = async () => await systemUnderTest.SearchByPostcode(MockPostcode, MockRepairType);
+            Func<Task> act = async () => await systemUnderTest.SearchByPostcode(MockPostcode, MockTenantRepairType);
 
             // Assert
             await act.Should().NotThrowAsync();
@@ -65,10 +72,10 @@ namespace HousingManagementSystemApi.Tests.GatewaysTests
         {
             // Arrange
             feedIteratorMock.Setup(_ => _.HasMoreResults).Returns(false);
-            containerResolverMock.Setup(x => x.Resolve(It.IsAny<string>())).Returns(containerMock.Object);
+            containerResolverMock.Setup(x => x.Resolve(It.IsAny<string>())).Returns(tenantContainerMock.Object);
             cosmosQueryHelperMock.Setup(_ => _.GetItemQueryIterator<PropertyAddress>(MockPostcode))
                 .Returns(feedIteratorMock.Object);
-            containerMock
+            tenantContainerMock
                 .Setup(c => c.GetItemQueryIterator<PropertyAddress>(
                     It.IsAny<QueryDefinition>(),
                     It.IsAny<string>(),
@@ -76,7 +83,7 @@ namespace HousingManagementSystemApi.Tests.GatewaysTests
                 .Returns(feedIteratorMock.Object);
 
             // Act
-            var results = await systemUnderTest.SearchByPostcode(MockPostcode, MockRepairType);
+            var results = await systemUnderTest.SearchByPostcode(MockPostcode, MockTenantRepairType);
 
             // Assert
             Assert.False(results.Any());
@@ -84,16 +91,16 @@ namespace HousingManagementSystemApi.Tests.GatewaysTests
 
         [Fact]
 #pragma warning disable CA1707
-        public async void Test_Addresses_Returned_When_Iterator_Results()
+        public async void Test_Tenant_Addresses_Returned_When_Iterator_Returns_Results()
 #pragma warning restore CA1707
         {
             // Arrange
             feedIteratorMock.Setup(_ => _.HasMoreResults).Returns(true);
-            var addressList = new List<PropertyAddress> { new() { CityName = "test", PostalCode = MockPostcode } };
-            var feedResponseMock = new Mock<FeedResponse<PropertyAddress>>();
-            feedResponseMock.Setup(x => x.GetEnumerator()).Returns(addressList.GetEnumerator());
+            var tenantAddressList = new List<PropertyAddress> { new() { BuildingName = TenantAddress, CityName = "test", PostalCode = MockPostcode } };
 
-            containerResolverMock.Setup(x => x.Resolve(It.IsAny<string>())).Returns(containerMock.Object);
+            feedResponseMock.Setup(x => x.GetEnumerator()).Returns(tenantAddressList.GetEnumerator());
+
+            containerResolverMock.Setup(x => x.Resolve(MockTenantRepairType)).Returns(tenantContainerMock.Object);
 
             feedIteratorMock
                 .Setup(f => f.ReadNextAsync(It.IsAny<CancellationToken>()))
@@ -106,7 +113,7 @@ namespace HousingManagementSystemApi.Tests.GatewaysTests
                 .Returns(feedIteratorMock.Object);
 
             // var containerMock = new Mock<Container>();
-            containerMock
+            tenantContainerMock
                 .Setup(c => c.GetItemQueryIterator<PropertyAddress>(
                     It.IsAny<QueryDefinition>(),
                     It.IsAny<string>(),
@@ -114,10 +121,50 @@ namespace HousingManagementSystemApi.Tests.GatewaysTests
                 .Returns(feedIteratorMock.Object);
 
             // Act
-            var results = await systemUnderTest.SearchByPostcode(MockPostcode, MockRepairType);
+            var results = await systemUnderTest.SearchByPostcode(MockPostcode, MockTenantRepairType);
 
             // Assert
-            Assert.Single(results);
+            // Assert.Single(results);
+            Assert.Equal(TenantAddress, results.FirstOrDefault().BuildingName);
+        }
+
+        [Fact]
+#pragma warning disable CA1707
+        public async void Test_Communal_Addresses_Returned_When_Iterator_Returns_Results()
+#pragma warning restore CA1707
+        {
+            // Arrange
+            feedIteratorMock.Setup(_ => _.HasMoreResults).Returns(true);
+            var communalAddressList = new List<PropertyAddress> { new() { BuildingName = CommunalAddress, CityName = "test", PostalCode = MockPostcode } };
+
+            feedResponseMock.Setup(x => x.GetEnumerator()).Returns(communalAddressList.GetEnumerator());
+
+            containerResolverMock.Setup(x => x.Resolve(MockCommunalRepairType)).Returns(communalContainerMock.Object);
+
+            feedIteratorMock
+                .Setup(f => f.ReadNextAsync(It.IsAny<CancellationToken>()))
+                .ReturnsAsync(feedResponseMock.Object)
+                .Callback(() => feedIteratorMock
+                    .Setup(f => f.HasMoreResults)
+                    .Returns(false));
+
+            cosmosQueryHelperMock.Setup(x => x.GetItemQueryIterator<PropertyAddress>(MockPostcode))
+                .Returns(feedIteratorMock.Object);
+
+            // var containerMock = new Mock<Container>();
+            communalContainerMock
+                .Setup(c => c.GetItemQueryIterator<PropertyAddress>(
+                    It.IsAny<QueryDefinition>(),
+                    It.IsAny<string>(),
+                    It.IsAny<QueryRequestOptions>()))
+                .Returns(feedIteratorMock.Object);
+
+            // Act
+            var results = await systemUnderTest.SearchByPostcode(MockPostcode, MockCommunalRepairType);
+
+            // Assert
+            // Assert.Single(results);
+            Assert.Equal(CommunalAddress, results.FirstOrDefault().BuildingName);
         }
     }
 }
