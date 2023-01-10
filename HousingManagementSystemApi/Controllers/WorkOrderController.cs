917b5d64801b114490fc7fd7ddf192ac4353c52f
Mark Warriner <mark.warriner@madetech.com>
2023-01-10 11:07:08 +0000

    Add Work order Controller Co-authored-by: Bhavesh-Patel <bhavesh-patel@users.noreply.github.com>

---
+++ HousingManagementSystemApi/Controllers/WorkOrderController.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Threading.Tasks;
+using HousingManagementSystemApi.UseCases;
+using Microsoft.AspNetCore.Mvc;
+using Sentry;
+
+namespace HousingManagementSystemApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WorkOrderController : ControllerBase
+    {
+        private readonly ICreateWorkOrderUseCase createWorkOrderUseCase;
+        public WorkOrderController(ICreateWorkOrderUseCase createWorkOrderUseCase)
+        {
+            this.createWorkOrderUseCase = createWorkOrderUseCase;
+        }
+
+        [HttpPost]
+        [Route(nameof(CreateWorkOrder))]
+        public async Task<IActionResult> CreateWorkOrder([FromQuery] string locationId, [FromQuery] string sorCode)
+        {
+            try
+            {
+                var result = await createWorkOrderUseCase.Execute(locationId, sorCode);
+                return Ok(result);
+            }
+            catch (Exception e)
+            {
+                SentrySdk.CaptureException(e);
+                return StatusCode(500, e.Message);
+            }
+        }
+    }
+}
