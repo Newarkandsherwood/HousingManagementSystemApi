b2e392221014ec19f2a02752151e5be961a4d6b6
Mark Warriner <mark.warriner@madetech.com>
2023-01-12 09:52:57 +0000

    Add CapitaService Files

---
+++ HousingManagementSystemApi/Services/CapitaService.cs
@@ -0,0 +1,8 @@
+namespace HousingManagementSystemApi.Services;
+
+using System.Threading.Tasks;
+
+public class CapitaService : ICapitaService
+{
+    public Task<string> LogJob(string description, string locationId, string sorCode) => throw new System.NotImplementedException();
+}
